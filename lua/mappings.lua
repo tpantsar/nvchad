4202ef1ff902a46a814c4b0306a11d03bebe3ad9
tpantsar <tomi.pantsar@gmail.com>
2025-08-02 21:45:11 +0300

    edit mappings and options

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,121 @@
-require "nvchad.mappings"
+-- https://github.com/JazzyGrim/dotfiles/blob/master/.config/nvim/lua/config/keymaps.lua
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
--- add yours here
+local function map(mode, lhs, rhs, desc)
+  vim.keymap.set(mode, lhs, rhs, vim.tbl_extend("force", { noremap = true, silent = true }, { desc = desc }))
+end
 
-local map = vim.keymap.set
+keymap.set("n", "x", '"_x')
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- Substitution
+keymap.set("n", "gs", ":%s~~", opts)
+keymap.set("v", "gs", ":%s~~", opts)
+keymap.set("n", "gS", [[:%s///gc<Left><Left><Left><Left>]], { desc = "Replace in buffer with confirm" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- map("n", "<leader>s", [[:%s///gc<Left><Left><Left>]], "Substitute in entire buffer")
+-- map("v", "<leader>s", [[:<C-u>s///g<Left><Left>]], "Substitute in visual selection")
+-- keymap.set("n", "gs", "zyiw:s/<C-r>z//g<Left><Left>", opts)
+-- keymap.set("n", "gS", "zyiw:%s/<C-r>z//gc<Left><Left><Left>", opts)
+-- keymap.set("v", "gs", "zy:s/<C-r>z//g<Left><Left>", opts)
+
+-- Increment/decrement
+-- keymap.set("n", "+", "<C-a>")
+-- keymap.set("n", "-", "<C-x>")
+keymap.set("n", "<C-a>", "ggVG", opts)   -- Select all text
+keymap.set("n", "<leader>A", "VG", opts) -- Select all text from current line to end of file
+
+-- Files
+keymap.set("n", "<C-w>", ":w<CR>", opts)          -- Save in normal mode
+keymap.set("i", "<C-w>", "<Esc>:w<CR>", opts)     -- Save in insert mode
+keymap.set("n", "<leader>wq", ":wq<CR>", opts)    -- Write and quit
+keymap.set("n", "<leader>wQ", ":wqa<CR>", opts)   -- Write all windows and quit
+keymap.set("n", "QQ", ":q!<CR>", opts)            -- Quit window without saving
+keymap.set("n", "<leader>QQ", ":qa!<CR>", opts)   -- Quit all windows without saving
+keymap.set("n", "<leader>q", ":q<CR>", opts)      -- Quit current window
+keymap.set("n", "<leader>Q", ":qa<CR>", opts)     -- Quit all windows
+keymap.set("n", "E", "$", opts)                   -- Go to end of line
+keymap.set("n", "B", "^", opts)                   -- Go to beginning of line
+keymap.set("n", "<leader>p", "mzA<Esc>p`z", opts) -- Paste at end of line
+keymap.set("n", "ye", "y$", opts)                 -- Yank a text to the end of line
+
+-- Tabs
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "tw", ":tabclose<CR>", opts)
+
+-- Buffers
+keymap.set("n", "<Tab>", ":bnext<CR>", opts)   -- Next buffer
+keymap.set("n", "<C-Tab>", ":bnext<CR>", opts) -- Next buffer
+keymap.set("n", "<S-Tab>", ":bprev<CR>", opts) -- Previous buffer
+keymap.set("n", "<leader>bd", ":bd<CR>", opts) -- Close buffer
+
+-- Split window
+keymap.set("n", "ss", ":vsplit<CR>", opts)
+keymap.set("n", "sv", ":split<CR>", opts)
+
+-- Move between windows
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-S-h>", "<C-w><")
+keymap.set("n", "<C-S-l>", "<C-w>>")
+keymap.set("n", "<C-S-k>", "<C-w>+")
+keymap.set("n", "<C-S-j>", "<C-w>-")
+
+-- Always keep the cursor centered when searching in Neovim
+keymap.set("n", "n", "nzzzv", opts)
+keymap.set("n", "N", "Nzzzv", opts)
+keymap.set("n", "*", "*zzzv", opts)
+keymap.set("n", "#", "#zzzv", opts)
+keymap.set("n", "g*", "g*zzzv", opts)
+keymap.set("n", "g#", "g#zzzv", opts)
+
+-- Navigate back and forward in the jumplist using Alt+h/l or Alt+i/o
+keymap.set("n", "<M-h>", "<C-o>", opts)      -- Previous position
+keymap.set("n", "<M-l>", "<C-i>", opts)      -- Next position
+keymap.set("n", "<M-i>", "<C-o>", opts)      -- Previous position
+keymap.set("n", "<M-o>", "<C-i>", opts)      -- Next position
+
+keymap.set("n", "<C-f>", "/", opts)          -- Forward search
+keymap.set("n", "<C-b>", "?", opts)          -- Backwards search
+
+keymap.set("n", "<A-k>", ":m .-2<CR>", opts) -- Move line up
+keymap.set("n", "<A-j>", ":m .+1<CR>", opts) -- Move line down
+
+-- Move selection up and down
+keymap.set("v", "<A-k>", ":m '<-2<CR>gv", opts) -- Move up without re-indenting
+keymap.set("v", "<A-j>", ":m '>+1<CR>gv", opts) -- Move down without re-indenting
+
+-- Clear highlights on search when pressing <Esc> in normal mode, see `:help hlsearch`
+keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Cut selected text without copying it to the default register (black hole register)
+keymap.set("v", "<leader>d", '"_d', opts)
+
+keymap.set("v", "<leader>y", '"+y', opts) -- Copy to clipboard
+keymap.set("n", "<leader>P", '"+P', opts) -- Paste before cursor
+-- keymap.set("n", "<leader>p", '"+p', opts) -- Paste from clipboard
+
+keymap.set("n", "<leader>ff", function()
+  require("favlist"):pick()
+end, { desc = "Pick from favlist" })
+
+keymap.set("n", "<leader>o", function()
+  require("favlist"):pick()
+end, { desc = "Pick from favlist" })
+
+keymap.set("n", "<leader>S", function()
+  require("favlist"):save()
+end, { desc = "Save to favlist" })
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+-- nvimtree
+keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,68 @@
-require "nvchad.options"
+-- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 
--- add yours here!
+vim.cmd("set expandtab")                    -- Use spaces instead of tabs
+vim.cmd("set number relativenumber")        -- Show absolute line number for current line, relative for others
+vim.cmd("set cursorline")                   -- Highlight the line where the cursor is
+vim.cmd("set hidden")                       -- Allow switching buffers without saving
+vim.cmd("set clipboard=unnamedplus")        -- Use system clipboard for all operations
+vim.cmd("set tabstop=2")                    -- Number of spaces that a <Tab> in the file counts for
+vim.cmd("set softtabstop=2")                -- Number of spaces a <Tab> feels like while editing
+vim.cmd("set shiftwidth=2")                 -- Number of spaces to use for autoindent
+vim.cmd("set nocompatible")                 -- Disable Vi compatibility (enables many Vim features)
+vim.cmd("set hls")                          -- Highlight search results (equivalent to :set hlsearch)
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+vim.cmd("set termguicolors")                -- Enable true color support
+vim.cmd("set signcolumn=yes")               -- Keep signcolumn on to avoid shifting text
+vim.cmd("set scrolloff=10")                 -- Minimal number of screen lines to keep above/below cursor
+vim.cmd("set sidescrolloff=10")             -- Same for horizontal scrolling
+vim.cmd("set laststatus=3")                 -- Global statusline (Neovim only)
+vim.cmd("set cmdheight=1")                  -- Set space for the command line
+
+vim.cmd("set smartindent")                  -- Smart auto-indenting
+vim.cmd("set updatetime=300")               -- Faster CursorHold events (for plugins like LSP)
+vim.cmd("set timeoutlen=500")               -- Shorten mapped key wait time
+vim.cmd("set completeopt=menuone,noselect") -- Better completion experience
+vim.cmd("set shortmess+=c")                 -- Avoid showing extra messages during completion
+
+vim.cmd("set incsearch")                    -- Show match while typing
+
+-- Leader key is Space
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Basically vim.o allows you to set global vim options, but not local buffer vim options.
+-- vim.opt allows a much more expansive API that can handle local and global vim options.
+-- https://www.reddit.com/r/neovim/comments/qgwkcu/difference_between_vimo_and_vimopt/?rdt=56644
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+vim.opt.background = "dark"       -- dark, light
+vim.opt.ignorecase = true         -- Ignore case in searches
+vim.opt.smartcase = true          -- If the search contains uppercase letters, make it case-sensitive
+vim.opt.wrap = false              -- Disable line wrapping
+vim.opt.swapfile = false          -- Prevents Neovim from creating swap files
+vim.opt.clipboard = "unnamedplus" -- Always yank to the system clipboard
+vim.opt.autoread = true           -- auto-reload from disk
+vim.opt.writeany = true           -- Allow overwriting even if changed outside
+vim.opt.backupcopy = "yes"        -- Write directly to the file instead of via temp/rename
+
+vim.scriptencoding = "utf-8"
+vim.fileencoding = "utf-8"
+
+-- https://www.youtube.com/watch?v=V070Zmvx9AM&t=154s
+vim.opt.encoding = "utf-8"
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitkeep = "cursor"
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = "a"
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
+-- Prepends asterisk (*) in block comments
+vim.opt.formatoptions:append({ "r" })
